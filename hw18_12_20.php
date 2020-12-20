c3b31999be636ebc4dcbce8c644ff04ada49b718
Dmitriy Bashkirov <dimasmgn333@gmail.com>
2020-12-21 00:03:47 +0500

    Дмитрий Башкиров

---
+++ hw18_12_20.php
@@ -0,0 +1,18 @@
+<?
+echo $name = "Дмитрий Башкиров<br/>";
+echo $age = "19 лет";
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <p>Дмитрий Башкиров</p>
+    <p>19 лет</p>
+</body>
+</html>
